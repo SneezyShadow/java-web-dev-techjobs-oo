23adc3364263b5a1074d3bc331d349ffdb4bf079
SneezyShadow <sneezyshadow@gmail.com>
2019-12-02 04:42:07 -0600

    Third Commit of the assignment, stuck on part 2 of part 4, but continuing.

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTests.java
@@ -0,0 +1,34 @@
+package org.launchcode.techjobs_oo.Tests;
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+import org.junit.Assert;
+
+
+public class JobTests {
+
+    @Test
+    public void testSettingJobId(){
+        Job testId = new Job();
+        Job testId2 = new Job();
+        Assert.assertEquals(1,testId.getId());
+        Assert.assertEquals(2, testId2.getId());
+    }
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assert.assertEquals("Product tester", testJob.getName());
+        Assert.assertEquals("ACME", testJob.getEmployer());
+        Assert.assertEquals("Desert", testJob.getLocation());
+        Assert.assertEquals("Quality control", testJob.getPositionType());
+        Assert.assertEquals("Persistence", testJob.getCoreCompetency());
+
+    }
+    @Test
+    public void testJobsForEquality(){
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Assert.assertFalse(testJob.equals(testJob2));
+    }
+
+}
