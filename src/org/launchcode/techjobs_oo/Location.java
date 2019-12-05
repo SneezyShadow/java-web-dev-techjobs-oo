43f4fff3da000fab6db08e895ffe8b7d976aae5a
SneezyShadow <sneezyshadow@gmail.com>
2019-12-05 10:07:45 -0600

    Finished

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -3,51 +3,12 @@ package org.launchcode.techjobs_oo;
 import java.util.Objects;
 
 public class Location extends JobField{
-    private int id;
-    private static int nextId = 1;
-    private String value;
 
-    public Location() {
-        id = nextId;
-        nextId++;
-    }
-
-public Location(String Value){
-        this();
-        this.value = Value;
+    public Location(String value){
+        super(value);
 }
-
-    // Custom toString, equals, and hashCode methods:
-
     @Override
     public String toString() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Location)) return false;
-        Location location = (Location) o;
-        return getId() == location.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+        return this.getValue();
     }
 }
